bf16419314e85e9db78efeef64571a34b5d94b08
DylanFarkas <dylan.farkas@correounivalle.edu.co>
2024-11-23 15:03:54 -0500

    .

--- IA/nodo.py
+++
@@ -1,7 +0,0 @@
-class Nodo:
-    def __init__(self, tablero, jugador, profundidad, utilidad):
-      self.tablero = tablero
-      self.jugador = jugador
-      self.profundiad = profundidad
-      self.utilidad = utilidad
-    
